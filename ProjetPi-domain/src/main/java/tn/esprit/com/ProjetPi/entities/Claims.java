74eed0280b9b619629558cf7b51070b53155e66b
hmed007 <36707911+hmed007@users.noreply.github.com>
2019-01-01 14:35:19 +0100

    Add files via upload

---
+++ ProjetPi-domain/src/main/java/tn/esprit/com/ProjetPi/entities/Claims.java
@@ -0,0 +1,5 @@
+package tn.esprit.com.ProjetPi.entities;
+
+public class Claims {
+
+}
---
+++ ProjetPi-domain/src/main/java/tn/esprit/com/ProjetPi/entities/TypeLeave.java
@@ -0,0 +1,5 @@
+package tn.esprit.com.ProjetPi.entities;
+
+public enum TypeLeave {
+	Maternity, Vacation, Disease, Autre
+}
